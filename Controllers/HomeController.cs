ed4a47ca4411e02b48c71353807ae0a596696c1f
imam <147700388+Allspark66@users.noreply.github.com>
2026-04-26 23:47:59 +0400

    tapsirig9

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication10.Models;
+using Microsoft.Data.Sqlite;
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<StudentCourse> list = new List<StudentCourse>();
+            using (var con = new SqliteConnection(@"Data Source=C:\Users\Memmedov\Documents\university\c#\tapsirig9\database.db"))
+            {
+                con.Open();
+                var cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT StudentName, CourseName FROM Student";
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        list.Add(new StudentCourse
+                        {
+                            StudentName = dr["StudentName"].ToString(),
+                            CourseName = dr["CourseName"].ToString(),
+                            CurrentCount = 1,
+                            Limit = 0
+                        });
+                    }
+                }
+            }
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string studentName, string courseName)
+        {
+            using (var con = new SqliteConnection(@"Data Source=C:\Users\Memmedov\Documents\university\c#\tapsirig9\database.db"))
+            {
+                con.Open();
+                var cmd = con.CreateCommand();
+                cmd.CommandText = "INSERT INTO Student (StudentName, CourseName) VALUES (@student, @course)";
+
+                cmd.Parameters.AddWithValue("@student", studentName);
+                cmd.Parameters.AddWithValue("@course", courseName);
+
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
